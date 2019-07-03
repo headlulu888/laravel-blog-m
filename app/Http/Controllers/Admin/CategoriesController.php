cb2a2cd354f1dcfde823e386fb8af371a2eb59f6
headlulu888 <headlulu888@gmail.com>
2019-07-03 23:36:47 +0300

    add index & create categories

---
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CategoriesController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all();
+
+        return view('admin.categories.index', compact('categories'));
+    }
+
+    public function create()
+    {
+        return view('admin.categories.create');
+    }
+
+}
